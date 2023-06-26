c02bb7fec0c82b648c17c508ada6e043162430f6
KABUYA <kabuya-sina@netcourrier.com>
2023-06-26 19:28:29 +0200

    header setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,56 @@
 import React, { useState } from 'react'
 import Logo from '../images/header.webp'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+import {FaBars, FaTimes} from 'react-icons/fa'
 import '../sass/layout/_header.scss'
 
 export const Header = () => {
+    const [burger, setBurger] = useState(false)
 
-  return (
-    <header>
-        <div className='header-container'>
-            <div className="header-logo">
-                <Link to='/'>
-                    <img src={Logo} alt="logo agence" />
-                </Link>
-            </div>
-            <nav>
-                <ul className='header-nav__ul'>
-                    <li>
-                        <NavLink href='../pages/Studio.js' to="/studio" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
-                            Studio
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink href='../pages/Realisation.js' to="/realisation" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
-                            Realisations
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink href='../pages/Expertises' to="/expertises" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
-                            Expertises
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink href='#' to="/" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
-                <div className="hamburger">
-                    <div className="bar" ></div>
-                    <div className="bar"></div>
-                    <div className="bar" ></div>
+    function handleBurger(){
+        setBurger(!burger)
+    }
+
+    return (
+        <header>
+            <div className='header-container'>
+                <div className="header-logo">
+                    <Link to='/'>
+                        <img src={Logo} alt="logo agence" />
+                    </Link>
                 </div>
-            </nav>
-        </div>
-    </header>
-  )
+                <nav>
+                    <ul className={ burger ? 'header-nav__ul active ' : ' header-nav__ul'}>
+                        <div className="burger-next" onClick={handleBurger}>
+                            {burger ? <FaTimes size={30}/> : '' }
+                        </div>
+                        <li>
+                            <NavLink href='../pages/Studio.js' to="/studio" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
+                                Studio
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink href='../pages/Realisation.js' to="/realisation" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
+                                Réalisations
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink href='../pages/Expertises' to="/expertises" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
+                                Expertises
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink href='#' to="/" className={(nav)  => (nav.isActive ? 'nav-active' : '')}>
+                                Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="hamburger" onClick={handleBurger}>
+                    {burger ? '' : <FaBars size={30}/> }
+                </div>
+            </div>
+        </header>
+    )
 }
 export default Header
