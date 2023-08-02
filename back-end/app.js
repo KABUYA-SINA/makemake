a18b26ed9745c0f3dd9c019a3f01d4a197f7b32f
KABUYA <kabuya-sina@netcourrier.com>
2023-08-02 16:45:37 +0200

    test

--- back-end/app.js
+++ back-end/app.js
@@ -34,9 +34,11 @@ app.use((req, res, next) => {
 
 //refresh Limite
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    windowMs: 15 * 60 * 1000,
+    max: 100,
     message: "Too many requests have been sent please try again later.",
+    standardHeaders: true,
+	legacyHeaders: false,
 })
 
 
